13fdd57a1468fa32eb49346e17226f86490e8824
danieltavida <128611124+danieltavida@users.noreply.github.com>
2023-10-11 11:25:42 -0300

    ....

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var mostrarMensagemBtn = document.getElementById("mostrarMensagem");
 
     mostrarMensagemBtn.addEventListener("click", function () {
-        mensagem.innerHTML = "Dia do Professor é o momento para valorizar e reconhecer a nobre missão desses mestres que tanto se dedicam à arte de ensinar." +
-        "Vamos honrá-los não apenas com palavras, mas com ações.!" +
-        "A cada educador, nosso respeito e gratidão." + 
-        "Que este dia seja repleto de reconhecimento, carinho e admiração por aqueles que iluminam nossas vidas com sabedoria e amor." +
+        mensagem.innerHTML = "Dia do Professor é o momento para valorizar e reconhecer a nobre missão desses mestres que tanto se dedicam à arte de ensinar. " +
+        "Vamos honrá-los não apenas com palavras, mas com ações.! " +
+        "A cada educador, nosso respeito e gratidão. " + 
+        "Que este dia seja repleto de reconhecimento, carinho e admiração por aqueles que iluminam nossas vidas com sabedoria e amor. " +
         
-        "Feliz Dia dos Professores!";
+        "Feliz Dia dos Professores! " ;
         
     });
 });
